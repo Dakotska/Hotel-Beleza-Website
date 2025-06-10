a4435b76b1c5be69425c08209533db7358d78708
Dakota2 <elevaluroberto@gmail.com>
2025-06-10 11:02:56 -0700

    Version 0.2.5

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,3 +1,14 @@
 function smoothChange() {
-    console.log("si")
+    divImages = document.querySelector("images");
+    imgLogin = document.querySelector("img-login");
+    imgRegister = document.querySelector("img-register");
+    console.log(divImages.classList())
+    /*if (divImages.classList.contains("login")) {
+        divImages.classList.remove("login");
+        divImages.classList.add("register");
+    } else if (divImages.classList.contains("register")) {
+        divImages.classList.remove("register");
+        divImages.classList.add("login");
+    }*/
+
 }
